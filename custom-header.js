92f8a2ffad8da27c86e8e32649d904e81f0adf13
Jim Thoburn <jim@jimthoburn.com>
2017-11-02 18:02:36 -0700

    Support custom images specified in the content of the page.

--- custom-header.js
+++ custom-header.js
@@ -129,6 +129,26 @@
 
     // Get a reference to the footer element
     // Update the HTML
+
+
+
+    // Check to see if the page HTML matches a certain pattern (the HTML that we expect to see on School Loop 1.0)
+    // If it does match, look for an image that appears right after the main headline
+    // If that image is found, update it’s URL from w700 to w2000
+    var image = document.querySelector(
+      '#container_content_home > .content_full > div > div[style="width: 100%; width: 970px"] > img[width="970"]:first-of-type'
+      + ', ' +
+      '#page_title + table > tbody > tr > td > #block_hub_main_b > .block_content_main > div > div[style="width: 100%; width: 350px"] > img[width="350"]:first-of-type'
+      + ', ' +
+      '#page_title + table > tbody > tr > td > #block_hub_main > .block_content_main > div > div[style="width: 100%; width: 465px"] > img[width="465"]:first-of-type'
+      + ', ' +
+      '#page_title + #block_wide_main > .block_content_main > div > div[style="width: 100%; width: 700px"] > img[width="700"]:first-of-type'
+    );
+    if (image) {
+      image.setAttribute('src', image.getAttribute('src').replace('w350', 'w2000').replace('w465', 'w2000').replace('w700', 'w2000').replace('w970', 'w2000'));
+    }
+
+
   });
 
 
