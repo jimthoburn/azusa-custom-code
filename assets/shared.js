a7d592d343c48dedfd8bd479c3b37aa92d45d03c
Jim Thoburn <jim@jimthoburn.com>
2017-11-15 13:26:19 -0800

    Update code to work without line breaks (for school loop)

--- assets/shared.js
+++ assets/shared.js
@@ -1,5 +1,5 @@
 
-  // OPTIONAL: Remove the custom style sheet, if the user is signed in
+  /* OPTIONAL: Remove the custom style sheet, if the user is signed in */
   (function() {
     var timer;
     function check() {
@@ -12,37 +12,35 @@
       }
     }
 
-    // Check frequently
+    /* Check frequently */
     timer = setInterval(check, 10);
 
-    // Check one last time when the DOM has loaded
+    /* Check one last time when the DOM has loaded */
     document.addEventListener('DOMContentLoaded', function() {
       check();
       clearInterval(timer);
     });
-    // Stop checking when everything has loaded
+    /* Stop checking when everything has loaded */
     document.addEventListener('load', function() {
       clearInterval(timer);
     });
 
-    // If all else fails, stop checking after five seconds
+    /* If all else fails, stop checking after five seconds */
     setTimeout(function() {
       clearInterval(timer);
     }, 5000);
   })();
 
 
-  // OPTIONAL: Increase the resolution of the image in the header, by changing its “src” attribute
+  /* OPTIONAL: Increase the resolution of the image in the header, by changing its “src” attribute */
   (function() {
-    if (!document.querySelector) return;
+    if (!document.querySelector) {
+      return;
+    }
 
 
     function updateImage() {
-      // Check to see if the page HTML matches a certain pattern (the HTML that we expect to see on School Loop 1.0)
-      // If it does match, look for an image that appears right after the main headline
-      // If that image is found, update it’s URL from wXXX to w2000
-
-      // These selectors were taken directly from the style sheet, “custom.css”
+      /* These selectors were taken directly from the style sheet, “custom.css” */
       var image = document.querySelector(
         '#container_content_home > .content_full > div > div[style="width: 100%; width: 970px"] > img[width="970"]:first-of-type'
         + ', ' +
@@ -61,80 +59,88 @@
 
         image.setAttribute('src', url);
       } else {
-        // If there’s no image, adjust the height of the header
-        document.body.classList.add('azusa-no-header-image')
+        /* If there’s no image, adjust the height of the header */
+        document.body.classList.add('azusa-no-header-image');
       }
     }
 
-    // function nextSiblingIsImage(element) {
-    //   var candidate = element.nextSibling;
-    //   var targetNodeType = Node.ELEMENT_NODE;
-    //   while (candidate && candidate.nodeType !== targetNodeType) {
-    //     candidate = candidate.nextSibling;
-    //   }
-    //   if (candidate && candidate.nodeName.toLowerCase() === 'img') return true;
-    // }
-
     var timer;
+    function elementLoaded(element) {
+      if (element.parentNode && element.parentNode.nextSibling) {
+        return true;
+      } else {
+        return elementLoaded(element.parentNode);
+      }
+    }
     function check() {
       var title = document.getElementById('page_title');
 
-      if (title && title.parentNode.nextSibling) {
+      if (title && elementLoaded(title)) {
         updateImage();
         clearInterval(timer);
         timer = undefined;
       }
     }
 
-    // Check frequently
+    /* Check frequently */
     timer = setInterval(check, 10);
 
-    // Check one last time when the DOM has loaded
+    /*  Check one last time when the DOM has loaded */
     document.addEventListener('DOMContentLoaded', function() {
       if (timer) {
-        check()
-        if (timer) clearInterval(timer);
+        check();
+        if (timer) {
+          clearInterval(timer);
+        }
       }
     });
 
-    // Stop checking when everything has loaded
+    /* Stop checking when everything has loaded */
     document.addEventListener('load', function() {
       if (timer) {
-        check()
-        if (timer) clearInterval(timer);
+        check();
+        if (timer) {
+          clearInterval(timer);
+        }
       }
     });
 
-    // If all else fails, stop checking after five seconds
+    /* If all else fails, stop checking after five seconds */
     setTimeout(function() {
       if (timer) {
-        check()
-        if (timer) clearInterval(timer);
+        check();
+        if (timer) {
+          clearInterval(timer);
+        }
       }
     }, 5000);
   })();
 
 
-  // OPTIONAL: Increase the resolution of all of the images at the beginning of the home page, and turn them into a slide show
+  /* OPTIONAL: Increase the resolution of all of the images at the beginning of the home page, and turn them into a slide show */
   document.addEventListener('DOMContentLoaded', function() {
 
-    // These selectors were taken directly from the style sheet, “custom.css”
+    /* These selectors were taken directly from the style sheet, “custom.css” */
     var images = document.querySelectorAll(
       '#container_content_home > .content_full > div > div[style="width: 100%; width: 970px"] > img[width="970"]'
     );
 
-    // If there’s only one image, stop here
-    if (images.length <= 1) return;
+    /* If there’s only one image, stop here */
+    if (images.length <= 1) {
+      return;
+    }
 
 
     var nextImage;
     var nextURL;
     for (var index = 0; index < images.length; index++) {
-      nextImage = images[index]
+      nextImage = images[index];
       nextURL = nextImage.getAttribute('src').replace('w970', 'w2000');
 
       nextImage.setAttribute('src', nextURL);
-      if (index !== 0) nextImage.style.opacity = 0;
+      if (index !== 0) {
+        nextImage.style.opacity = 0;
+      }
     }
 
     var currentIndex = 0;
@@ -142,7 +148,9 @@
       images[currentIndex].style.opacity = 0;
 
       currentIndex++;
-      if (currentIndex >= images.length) currentIndex = 0;
+      if (currentIndex >= images.length) {
+        currentIndex = 0;
+      }
 
       images[currentIndex].style.opacity = 1;
     }
@@ -150,54 +158,3 @@
     var timer;
     timer = setInterval(showNext, 7000);
   });
-
-
-  // Add a slideshow on the home page
-  /*
-  document.addEventListener('DOMContentLoaded', function() {
-
-    var images = [
-      'https://jimthoburn.github.io/azusa-custom-code/images/football.jpg',
-      'https://jimthoburn.github.io/azusa-custom-code/images/graduates.jpg',
-      'https://jimthoburn.github.io/azusa-custom-code/images/football-2.jpg',
-      'https://jimthoburn.github.io/azusa-custom-code/images/singers.jpg',
-      'https://jimthoburn.github.io/azusa-custom-code/images/singers-2.jpg',
-      'https://jimthoburn.github.io/azusa-custom-code/images/best-buddies.jpg',
-      'https://jimthoburn.github.io/azusa-custom-code/images/students.jpg'
-    ];
-
-    var cursor = 0;
-    var image = document.getElementById('container_home_header');
-    if (!image) return;
-
-    var preloader = new Image();
-    var imageListener = preloader.addEventListener('load', update);
-
-    var timer;
-    var stopped = false;
-    function update() {
-      if (stopped) return;
-
-      image.setAttribute('style', 'background-image: url(' + images[cursor] + ') !important');
-
-      if (timer) clearTimeout(timer);
-      timer = setTimeout(preloadNext, 7000);
-    }
-
-    function preloadNext() {
-      if (stopped) return;
-
-      cursor++;
-      if (cursor >= images.length) cursor = 0;
-      preloader.src = images[cursor];
-    }
-
-    timer = setTimeout(preloadNext, 7000);
-
-    window.azusa = window.azusa || {};
-    window.azusa.stopHomeSlideshow = function() {
-      if (timer) clearTimeout(timer);
-      stopped = true;
-    }
-  });
-  */
